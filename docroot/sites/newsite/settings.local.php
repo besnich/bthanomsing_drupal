617d21078dfd37eea700219d9e534330cd62db60
besnich <bthanomsing@northcrossgroup.com>
2017-11-22 19:41:14 -0500

    Second site created

---
+++ docroot/sites/newsite/settings.local.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bthanomsing
+ * Date: 11/20/17
+ * Time: 8:39 AM
+ */
+$databases['default']['default'] = array (
+  'database' => 'drupal_test2',
+  'username' => 'besnich',
+  'password' => '99Of8jU=',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
